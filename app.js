90220d0a4b26940d00e16c0302174c8441419c2c
Filip <2hoang7@gmail.com>
2023-05-23 16:22:40 +0200

    Updated port for Heroku deployment

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
