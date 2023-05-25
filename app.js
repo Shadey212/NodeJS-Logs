3f8fa03cc05fd89122a28899a9718211f64cba34
Filip <75810145+Shadey212@users.noreply.github.com>
2023-05-25 02:17:55 +0200

    Updated to include more detailed logging
    Including Geolocation, User Agent, more detailed Stack trace, etc.

--- app.js
+++ app.js
@@ -22,26 +22,45 @@ const generateLogs = async () => {
     const method = faker.random.arrayElement(httpMethods);
     const url = faker.internet.url();
     const status = faker.random.arrayElement([200, 201, 400, 404, 500]);
+    const ip = faker.internet.ip(); // Fake IP
+    const city = faker.address.city(); // Fake city
+    const latitude = faker.address.latitude(); // Fake latitude
+    const longitude = faker.address.longitude(); // Fake longitude
+    const userAgent = faker.internet.userAgent(); // Fake user agent
     const log = {
       method,
       url,
       status,
+      ip,
+      city,
+      geolocation: {
+        latitude,
+        longitude
+      },
+      userAgent,
       error: null,
       stack: null
     };
-
+    
     if (status >= 400) {
       log.error = faker.random.words();
-      log.stack = faker.random.words(5);
+      log.stack = `Error at ${faker.random.arrayElement(['functionA', 'functionB', 'functionC'])} in ${faker.system.fileName()} line ${faker.random.number(100)}`; // More detailed fake stack trace
     }
 
     // Send log to Logtail with the appropriate level
     if (status >= 500) {
-      await logger.error(log);
+      await logger.critical(log); // Custom log level: critical
     } else if (status >= 400) {
+      await logger.error(log);
+    } else if (status >= 300) {
       await logger.warn(log);
     } else {
       await logger.info(log);
+    }
+
+    if (status < 300) { // Debug level logging
+      log.debugMessage = faker.lorem.sentence();
+      await logger.debug(log);
     }
 
     io.emit('log'); // Emit log count event
