2e9c7093bf68413bae62497e2d064a487d5d365d
Filip <75810145+Shadey212@users.noreply.github.com>
2023-05-25 03:15:54 +0200

    Update app.js
    Back to timeout

--- app.js
+++ app.js
@@ -12,63 +12,73 @@ app.use(express.static('public'));
 
 let generating = false;
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const generateLogs = async () => {
   const logger = createLogger();
   const httpMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
 
   while (generating) {
-    const method = faker.random.arrayElement(httpMethods);
-    const url = faker.internet.url();
-    const status = faker.random.arrayElement([200, 201, 400, 404, 500]);
-    const ip = faker.internet.ip();
-    const city = faker.address.city();
-    const latitude = faker.address.latitude();
-    const longitude = faker.address.longitude();
-    const userAgent = faker.internet.userAgent();
+    try {
+      const method = faker.random.arrayElement(httpMethods);
+      const url = faker.internet.url();
+      const status = faker.random.arrayElement([200, 201, 400, 404, 500]);
+      const ip = faker.internet.ip();
+      const city = faker.address.city();
+      const latitude = faker.address.latitude();
+      const longitude = faker.address.longitude();
+      const userAgent = faker.internet.userAgent();
 
-    const log = {
-      method,
-      url,
-      status,
-      ip,
-      geolocation: {
-        city,
-        latitude,
-        longitude
-      },
-      userAgent,
-      error: null,
-      stack: null
-    };
+      const log = {
+        method,
+        url,
+        status,
+        ip,
+        geolocation: {
+          city,
+          latitude,
+          longitude
+        },
+        userAgent,
+        error: null,
+        stack: null
+      };
 
-    if (status >= 400) {
-      log.error = faker.random.words();
-      log.stack = `Error at ${faker.random.arrayElement(['functionA', 'functionB', 'functionC'])} in ${faker.system.fileName()} line ${faker.datatype.number({ min: 1, max: 100 })}`;
+      if (status >= 400) {
+        log.error = faker.random.words();
+        log.stack = `Error at ${faker.random.arrayElement(['functionA', 'functionB', 'functionC'])} in ${faker.system.fileName()} line ${faker.datatype.number({ min: 1, max: 100 })}`;
+      }
+
+      if (status >= 500) {
+        logger.fatal(log);
+      } else if (status >= 400) {
+        logger.error(log);
+      } else if (status >= 300) {
+        logger.warn(log);
+      } else if (status >= 200) {
+        logger.http(log); // Using .http method for http status codes
+      } else {
+        logger.info(log);
+      }
+
+      if (status < 300) {
+        log.debugMessage = faker.lorem.sentence();
+        logger.debug(log);
+      }
+
+      // Let's add more log levels
+      logger.verbose({ message: faker.lorem.sentence() });
+      logger.silly({ message: faker.lorem.sentence() });
+      logger.trace({ message: faker.lorem.sentence() });
+
+      io.emit('log');
+
+      // Add delay here
+      await delay(50); // 50ms delay
+    } catch (error) {
+      console.error('Error generating logs:', error);
+      generating = false;
     }
-
-    if (status >= 500) {
-      logger.fatal(log);
-    } else if (status >= 400) {
-      logger.error(log);
-    } else if (status >= 300) {
-      logger.warn(log);
-    } else if (status >= 200) {
-      logger.http(log); // Using .http method for http status codes
-    } else {
-      logger.info(log);
-    }
-
-    if (status < 300) {
-      log.debugMessage = faker.lorem.sentence();
-      logger.debug(log);
-    }
-
-    // Let's add more log levels
-    logger.verbose({ message: faker.lorem.sentence() });
-    logger.silly({ message: faker.lorem.sentence() });
-    logger.trace({ message: faker.lorem.sentence() });
-
-    io.emit('log');
   }
 };
 
