04835d76e4602e2d0fccdac9e4a43442f1d1f913
Filip <75810145+Shadey212@users.noreply.github.com>
2023-05-25 02:55:45 +0200

    Update logtail.config.js
    Added support for more Log levels

--- logtail.config.js
+++ logtail.config.js
@@ -5,9 +5,16 @@ const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN);
 
 function createLogger() {
   return {
-    info: (message) => logtail.info(message),
-    warn: (message) => logtail.warn(message),
     error: (message) => logtail.error(message),
+    warn: (message) => logtail.warn(message),
+    info: (message) => logtail.info(message),
+    debug: (message) => logtail.debug(message),
+    fatal: (message) => logtail.error({...message, level: 'fatal'}), // use .error method, but add 'level' field to indicate fatal error
+    // Similarly, you can add other log levels using the existing methods and the 'level' field
+    http: (message) => logtail.info({...message, level: 'http'}), // use .info method for http level
+    verbose: (message) => logtail.debug({...message, level: 'verbose'}), // use .debug method for verbose level
+    silly: (message) => logtail.debug({...message, level: 'silly'}), // use .debug method for silly level
+    trace: (message) => logtail.debug({...message, level: 'trace'}), // use .debug method for trace level
   };
 }
 
